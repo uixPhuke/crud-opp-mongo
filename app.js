4818c167e51f73b162e69a1a47debc722c80b311
Rohan Phu-ke <uixphuke@gmail.com>
2025-08-30 22:52:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, () => {
-    console.log('server is runnning LIVE on at PORT 3000')
+    console.log('server is runnning LIVE on at PORT _3000')
 })
